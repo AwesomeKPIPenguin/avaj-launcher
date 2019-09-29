b14fea0ebf6082ee91957e7a26cc4067f4a1b8b1
AwesomeKPIPenguin <dima.omelchenko99@gmail.com>
2019-09-29 16:32:21 +0300

    main merge

--- avaj-launcher/src/com/unit/Main.java
+++ avaj-launcher/src/com/unit/Main.java
@@ -1,6 +1,8 @@
 
 package com.unit;
 
+import com.unit.exceptions.WrongSimulationFileFormatException;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,6 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		WeatherTower wt;
+		Flyable flyable;
 		BufferedReader reader;
 		String line;
 		String[] params;
@@ -29,23 +32,24 @@ public class Main {
 			reader = new BufferedReader(new FileReader(args[0]));
 			if ((line = reader.readLine()) == null)
 				return;
-			simulationCount = Integer.parseInt(line);
+			try { simulationCount = Integer.parseInt(line); }
+			catch (Exception e) { throw new WrongSimulationFileFormatException(); }
 			while ((line = reader.readLine()) != null) {
 
 				params = line.split(" ");
-				wt.register(
-					AircraftFactory.newAircraft(
-						params[0],
-						params[1],
-						Integer.parseInt(params[2]),
-						Integer.parseInt(params[3]),
-						Integer.parseInt(params[4])
-					)
+				flyable = AircraftFactory.newAircraft(
+					params[0],
+					params[1],
+					Integer.parseInt(params[2]),
+					Integer.parseInt(params[3]),
+					Integer.parseInt(params[4])
 				);
+				flyable.registerTower(wt);
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 
-			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return ;
 		}
 
 		for (int i = 0; i < simulationCount; ++i)
